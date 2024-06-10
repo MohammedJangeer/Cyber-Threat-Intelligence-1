142f795317f9112e90a3530eb5cc91f187971b72
Mohammed Jangeer <mohammedjangeer23@icloud.com>
2024-06-10 16:44:32 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleContent(id) {
-    const content = document.getElementById(id);
-    if (content.classList.contains('hidden')) {
-        content.classList.remove('hidden');
-    } else {
-        content.classList.add('hidden');
-    }
-}
